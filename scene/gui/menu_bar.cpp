5db4cba0a1a26a34ae50bb04402a46e55b71eb9b
Kacper Gibas <kacperbogangibas@gmail.com>
2025-05-10 08:15:35 +0200

    Fix menu bar popup placement ignoring parent window content scale factor when windows are not embedded
    Co-authored-by: Stefen Heinz <stefan.heinz.02@gmail.com>

--- scene/gui/menu_bar.cpp
+++ scene/gui/menu_bar.cpp
@@ -128,10 +128,11 @@ void MenuBar::_open_popup(int p_index, bool p_focus_item) {
 		return;
 	}
 
+	float content_scale_factor = pm->is_embedded() ? 1.0f : get_window()->get_content_scale_factor();
+	Size2 scale = get_canvas_transform().get_scale() * content_scale_factor;
 	Rect2 item_rect = _get_menu_item_rect(p_index);
-	Size2 canvas_scale = get_canvas_transform().get_scale();
-	Point2 screen_pos = get_screen_position() + item_rect.position * canvas_scale;
-	Size2 screen_size = item_rect.size * canvas_scale;
+	Point2 screen_pos = get_screen_position() + item_rect.position * scale;
+	Size2 screen_size = item_rect.size * scale;
 
 	active_menu = p_index;
 
